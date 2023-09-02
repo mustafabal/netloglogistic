aa348673a226f41c41478d4ac86197d1f45d4b18
mustafabal <mustafa.bal@aytemiz.com.tr>
2023-09-02 14:10:42 +0300

    bu adımda dependcy katmanını classa tasıdım daha sonra bu  class .net core api içerisinde tanımlanacak , ihtiyac olması durumdan şifreleme eklendi

---
+++ Logistic/Business/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using Dataacces.Abstract;
+using Dataacces.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolver.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UsersManager>().As<IUsersService>();
+            builder.RegisterType<EfUsersDal>().As<IUsersDal>();
+            builder.RegisterType<OrderManager>().As<IOrderService>();
+            builder.RegisterType<EfOrderDal>().As<IOrderDal>();
+            builder.RegisterType<OrderProductManager>().As<IOrderProductService>();
+            builder.RegisterType<EfOrderProductDal>().As<IOrderProductDal>();
+            builder.RegisterType<DeliveryManager>().As<IDeliveryService>();
+            builder.RegisterType<EfDeliveryDal>().As<IDeliveryDal>();
+        }
+    }
+}
